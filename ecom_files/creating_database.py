66c2266504274e77c7e04c8b23d201d0edd8a390
IshfaqKhawaja <ishfaqkhawaja312@gmail.com>
2024-03-06 19:51:12 +0530

    Mar 6

--- ecom_files/creating_database.py
+++ ecom_files/creating_database.py
@@ -7,6 +7,7 @@ import sys
 from tqdm import tqdm
 import os
 import requests
+import pickle 
 # For Importing libraries from other directories
 current_dir = os.path.dirname(__file__)
 parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
@@ -18,10 +19,9 @@ from imagebind.models import imagebind_model
 def create_database(model, device):
     dataset_name = "amazon data"
     total_directories = os.listdir(f"{parent_dir}/{dataset_name}")
-    main_category = []
-    sub_category = []
+    main_category = {}
+    sub_category = {}
     for directory in total_directories:
-        embeddings = []
         try:
             file_path = f"{parent_dir}/{dataset_name}/{directory}"
             df = pd.read_csv(file_path)
@@ -29,7 +29,7 @@ def create_database(model, device):
                 try:  
                     row = df.iloc[i]
                     image = row['image']
-                    # Save Image to the directory
+                    # Save Image to the directory:
                     image_path = f"images/{i}.jpg"
                     with open(image_path, 'wb') as f:
                         f.write(requests.get(image).content)
@@ -38,20 +38,34 @@ def create_database(model, device):
                     # Delete the image
                     os.remove(image_path)
                     # print(embedding)
-                    embeddings.append(embedding)
+                    # embeddings.append(embedding)
+                    m = row["main_category"]
+                    s = row["sub_category"]
+                    if m not in main_category:
+                        main_category[m] = []
+                    if s not in sub_category:
+                        sub_category[s] = []
+                    main_category[m].append(embedding)
+                    sub_category[s].append(embedding)
+                    
+                    
                 except Exception as e:
                     print(e)
                     if os.path.exists(image_path):
                         os.remove(image_path)
                     pass
-            sub_category.append(torch.mean(torch.stack(embeddings), dim=0))               
         except Exception as e:
             print(e)
             pass
-        break
+        
 
-
-
+    # Save main category and subcategory embeddings
+    f = open("main_category_embeddings.pkl", "wb")
+    pickle.dump(main_category, f)
+    f.close()
+    f = open("sub_category_embeddings.pkl", "wb")
+    pickle.dump(sub_category, f)
+    f.close()
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -60,4 +74,13 @@ if __name__ == "__main__":
     model.eval()
     model.to(device)
     create_database(model, device)
+    # Load Main category and sub category:
+    # f = open("main_category_embeddings.pkl", "rb")
+    # main_category = pickle.load(f)
+    # f.close()
+    # f = open("sub_category_embeddings.pkl", "rb")
+    # sub_category = pickle.load(f)
+    # f.close()
     
+    # print(main_category)
+    # print(sub_category)
